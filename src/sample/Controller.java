7e7e937538a790fc1e3afbec295c06507c4fb70b
luisabrahamaquinocastillo <luis.aquino.135769@gmail.com>
2018-04-30 00:13:47 -0500

    Nueva actualización 00:13 am

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,31 @@
 package sample;
 
+import javafx.application.Platform;
+import javafx.scene.input.MouseEvent;
+
 public class Controller {
+
+    //Metodo para controlar el cierre de la aplicación y corte procesos
+    public void exitAplicacionBoton(MouseEvent event){
+        Platform.exit();
+        System.exit(0);
+    }
+
+    /*Metodo para controlar la aparicion de los paneles correspondientes
+    * 1.- Usuario
+    * 2.- Localidad
+    * 3.- Educación */
+//  ( 1 )
+    public void usuarioAplicacionBoton(MouseEvent event){
+
+    }
+//  ( 2 )
+    public void localidadAplicacionBoton(MouseEvent event){
+
+    }
+//  ( 3 )
+    public void educacionAplicacionBoton(MouseEvent event){
+
+    }
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,18 +1,46 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
+
+    private double xOffset;
+    private double yOffset;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        //Control del evento click dentro del formulario para poder movilizarlo
+        root.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                xOffset=event.getSceneX();
+                yOffset=event.getSceneY();
+            }
+        });
+        //Control para movimineto del formulario sin necesidad del marco
+        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                primaryStage.setX(event.getScreenX()-xOffset);
+                primaryStage.setY(event.getScreenY()-yOffset);
+            }
+        });
+        //Para remover el decorado de la interfaz de usuario. Paso 13 del documento
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 386, 400));
+        //Creamos un objeto de tipo Escena
+        Scene scene=new Scene(root);
+        scene.setFill(Color.TRANSPARENT);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
